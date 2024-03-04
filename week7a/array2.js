6a40e3f0002c408536e4dcf3bc4b8cd5a9b74a1c
darkel7 <156670314+darkel7@users.noreply.github.com>
2024-03-04 10:00:08 +0100

    Add files via upload

---
+++ week7a/array2.js
@@ -0,0 +1,34 @@
+const numbers = [12, 34, 76, 123, 56, 8, 2, 988, 90, 345]
+
+
+max = Math.max(...numbers)
+min = Math.min(...numbers)
+divisible = numbers.filter(num => num % 2 ===0).length
+age = sum/numbers.length
+greater = numbers.filter(num => num > 50)
+order = numbers.slice().sort((a,b)=>a-b);
+even = numbers.every(num => num % 2 ===0)
+index = numbers.indexOf(76)
+order2 = numbers.slice().reverse()
+
+
+
+
+
+console.log(max)
+console.log(min)
+console.log(divisible)
+console.log(age)
+console.log(greater)
+console.log(order)
+console.log(even)
+console.log(index)
+console.log(order2)
+console.log
+
+
+
+
+
+const names = ["Alice","Bob","Charlie","Darlington","Emma","Fiona","George","Hannah","Joy","Julia"]
+
---
+++ week7a/arrays.js
@@ -0,0 +1,9 @@
+const arr1 = []
+const arr2 = new Array()
+const students = 'adebare darlington kosi'.split('')
+
+console.log(students(0))
+console.log(students.length)
+
+students.push('joshua')
+ students.pop
